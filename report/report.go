0d28686b2043756142d4641f3c422eaa171e9f42
gabrielpalacio <gabriel.palacio@bexsbanco.com.br>
2023-04-16 18:24:46 -0300

    Adds report struct

---
+++ report/report.go
@@ -0,0 +1,37 @@
+package report
+
+import (
+	"fmt"
+	"github.com/ggpalacio/quake-game-log-analytics/game"
+	"github.com/ggpalacio/quake-game-log-analytics/logfile"
+)
+
+type GameReport map[string]*game.Game
+
+func New(logFile *logfile.LogFile) GameReport {
+	var currentGame *game.Game
+	var currentGameIndex int
+	games := make(GameReport)
+
+	for _, logLine := range logFile.Lines {
+		if logLine.IsInitGame() {
+			currentGame = new(game.Game)
+			currentGameIndex++
+
+			gameKey := fmt.Sprintf("game_%s", fmt.Sprintf("%03d", currentGameIndex))
+			games[gameKey] = currentGame
+		}
+		if currentGame == nil {
+			continue
+		}
+		if playerID, playerName := logLine.ClientUserinfoChanged(); playerID != 0 {
+			currentGame.AddPlayer(playerName)
+			continue
+		}
+		if killer, killed, deathCause := logLine.Kill(); killer != "" {
+			currentGame.RegisterKill(killer, killed, deathCause)
+			continue
+		}
+	}
+	return games
+}
